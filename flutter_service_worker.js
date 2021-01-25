d558de5bfd69e06986f6e89a52962521068ab228
K110100100 <bluelasergames@gmail.com>
2021-01-25 10:12:59 +0200

    Update 1.1
    Added new features!
    - Added a game mode with note names instead of letters
    - Timer increased to 60 seconds

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "73e2b973a22501de89b922754c2df0bb",
-"index.html": "15edb7d431f4ec87a8401a7d15a99d82",
-"/": "15edb7d431f4ec87a8401a7d15a99d82",
-"main.dart.js": "9286eb8defca6046d90ab3105bced132",
+"index.html": "7ef0098e89b66f0629b2f829ca584da6",
+"/": "7ef0098e89b66f0629b2f829ca584da6",
+"main.dart.js": "9aa720da1b1a17224952bc2b93184032",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",

96e43d2f1887596c5923056fa9de7f9c4c34f010
K110100100 <bluelasergames@gmail.com>
2021-02-01 12:43:33 +0200

    Update 1.2
    - Added a new game mode
    - Cleaned up some code

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "73e2b973a22501de89b922754c2df0bb",
-"index.html": "7ef0098e89b66f0629b2f829ca584da6",
-"/": "7ef0098e89b66f0629b2f829ca584da6",
-"main.dart.js": "9aa720da1b1a17224952bc2b93184032",
+"index.html": "c2c3e0379a60a27706fdbc7172e1fa30",
+"/": "c2c3e0379a60a27706fdbc7172e1fa30",
+"main.dart.js": "4e491fe7d48bcc339f29d4e805680dd1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
